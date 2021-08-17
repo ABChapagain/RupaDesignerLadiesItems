f8993634e681dcc69ad03e6dbb909869d2510fdd
Achyut Chapagain <71538964+ABChapagain@users.noreply.github.com>
2021-08-17 22:36:13 +0545

    Add files via upload

--- js/navbar.js
+++ js/navbar.js
@@ -13,9 +13,3 @@ cancelBtn.addEventListener('click', () => {
     menuBtn.classList.remove("hide");
     cancelBtn.classList.remove("show");
 });
-
-window.addEventListener('scroll', function () {
-    let header = document.querySelector(".navbar");
-    let windowPosition = window.scrollY > 0;
-    header.classList.toggle("scrolling-active", windowPosition);
-})
