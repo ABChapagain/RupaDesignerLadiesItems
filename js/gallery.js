e5bc80b04f20e4a0bef889926d3945f49ea501c4
Achyut Chapagain <71538964+ABChapagain@users.noreply.github.com>
2021-08-18 21:17:33 +0545

    Add files via upload

---
+++ js/gallery.js
@@ -0,0 +1,11 @@
+/* curator-feed-default-feed-layout */
+(function () {
+    var i, e, d = document,
+        s = "script";
+    i = d.createElement("script");
+    i.async = 1;
+    i.src = "https://cdn.curator.io/published/e49a2cc2-671b-4be0-a8e5-de258e9d09e7.js";
+    e = d.getElementsByTagName(s)[0];
+    e.parentNode.insertBefore(i, e);
+})();
+
--- js/main.js
+++ js/main.js
@@ -1,1 +1,2 @@
 document.write(`<script src="js/navbar.js"></script>`);
+document.write(`<script src="js/gallery.js"></script>`);
