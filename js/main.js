c07e6257ae10f561242eea3216fdb663dfecaff8
Achyut Chapagain <71538964+ABChapagain@users.noreply.github.com>
2021-08-21 10:45:48 +0545

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,5 +1,24 @@
+
+
 document.write(`<script src="js/navbar.js"></script>`);
 document.write(`<script src="js/gallery.js"></script>`);
 document.write(`<script src="js/our-team.js"></script>`);
 document.write(`<script src="js/testimonials.js"></script>`);
 document.write(`<script src="js/faq.js"></script>`);
+document.write(`<script src="js/messenger.js"></script>`);
+
+
+$('a, .scroll').on('click', function(e) {
+    if(this.hash != '') {
+      e.preventDefault();
+  
+      const hash = this.hash;
+  
+      $('html, body').animate(
+        {
+        scrollTop: $(hash).offset().top - 50
+        },
+        800
+      );
+    }
+  });
